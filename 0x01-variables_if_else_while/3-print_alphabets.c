76a0da169f319d17ce4a890a94c12e89d827d011
Immeleo <leonardmwilu139@gmail.com>
2021-02-04 19:12:55 +0000

    third commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - returns 0
+*
+* Description - main returns 0
+* putchar - Prints each letter of the alphabet in lowercase and uppercase
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
