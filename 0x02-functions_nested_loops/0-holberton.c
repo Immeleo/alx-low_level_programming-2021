3f7e0266bdbcec35e9b6483ee782a7b675c9170c
Immeleo <leonardmwilu139@gmail.com>
2021-02-09 12:35:02 +0000

    update task o commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include "holberton.h"
+
 /*
-* main - print Holberton.
-* as simple program
-* Return : always zero.
+*
+* main - Entry point
+*
+* Return:1
 */
 int main(void)
 {
-_putchar("Holberton\n");
+char text_printed[9] = "Holberton";
+int count;
+
+for (count = 0; count < 9; count++)
+{
+_putchar(text_printed[count]);
+}
+_putchar('\n');
 return (0);
 }
