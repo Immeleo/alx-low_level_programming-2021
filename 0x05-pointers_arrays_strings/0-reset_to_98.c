abd651daa081fbde39e916d10492484ce3951a0d
Immeleo <leonardmwilu139@gmail.com>
2021-02-17 08:11:14 +0000

    task 0 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/*
+*
+*reset_to_98 - main function
+*@n : our pointer
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Understanding:
+    C - Arrays
+    C - Pointers
+    C - Strings
