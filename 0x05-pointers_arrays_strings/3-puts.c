e43f4454f93e4e4254928c225a98b485a49902e2
Immeleo <leonardmwilu139@gmail.com>
2021-02-17 10:03:54 +0000

    task 3 commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,7 @@
+
+
 #include "holberton.h"
+
 /*
 *
 * _puts - prints a string, followed by a new line, to stdout
@@ -11,7 +14,6 @@ void _puts(char *str)
 {
 
 int i;
-
 for (i = 0; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
