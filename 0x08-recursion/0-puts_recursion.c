d7c707350916d0031eba8f37eda26c41876e224c
Immeleo <leonardmwilu139@gmail.com>
2021-02-24 14:15:30 +0000

    task 0 update commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
-/*
-*a function to print a string
-*
-*
-*/
-void _puts_recursion(char *s){
-char *s= 'Welcome home';
+#include "holberton.h"
 
-return (*s);
+/*
+*
+*_puts_recursion - prints a string followed by new line
+*@s: string to be print
+*
+*Return: Always 0
+*/
+
+void _puts_recursion(char *s)
+{
+if (s[0] !='\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
