919e55efadb9b3bb2ef1a8bc1e7594be273d4494
Immeleo <leonardmwilu139@gmail.com>
2021-03-01 16:35:08 +0000

    new commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/*
+*
+* main - Print all arguments received.
+* @argc: Number of arguments
+* @argv: Array pointer to string
+* Return: Returns Always (0), Success
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
